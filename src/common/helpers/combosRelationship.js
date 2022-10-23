4a170036080fc265737083154afc64b4bc26e611
Bill Gatos <ironmorew@gmail.com>
2022-10-23 18:39:07 -0400

    Updating Images

--- src/common/helpers/combosRelationship.js
+++ src/common/helpers/combosRelationship.js
@@ -1,89 +1,73 @@
-import Combo1 from "../../public/combos/Combo Mixto 1.jpeg"
-import Combo2 from "../../public/combos/Combo Mixto 2.jpeg"
-import Combo3 from "../../public/combos/Combo Mixto 3.jpeg"
-import Combo4 from "../../public/combos/Combo Mixto 4.jpeg"
-import Combo5 from "../../public/combos/Combo Mixto 5.jpeg"
-import Combo6 from "../../public/combos/Combo Mixto 6.jpeg"
-import Combo7 from "../../public/combos/Combo Mixto 7.jpeg"
-import Combo8 from "../../public/combos/Combo Mixto 8.jpeg"
-import Combo9 from "../../public/combos/Combo Mixto 9.jpeg"
-import ComboFrito from "../../public/combos/Combo Pollo Frito.jpeg"
-import PolloAceitePolloRes from "../../public/combos/Combo Aceite Pollo Res.jpeg"
-
-import ComboCerdo from "../../public/combos/Combo Básico Cerdo.jpeg"
-import ComboRes from "../../public/combos/Combo Básico Res.jpeg"
-import ComboPollo from "../../public/combos/Combo Básico Pollo.jpeg"
-
 const combos = [
     {
         name: "Combo Mixto 1",
         price: "105",
-        image: Combo1,
+        image: "https://res.cloudinary.com/dq98mlb66/image/upload/v1666563055/Combo_Mixto_1_zmzsrk.jpg",
         contains: ["Mixto","Pollo"]
     },{
         name: "Combo Mixto 2",
         price: "115",
-        image: Combo2,
+        image: "https://res.cloudinary.com/dq98mlb66/image/upload/v1666563058/Combo_Mixto_2_rrmhz6.jpg",
         contains: ["Mixto","Pollo", "Cerdo"]
     },{
         name: "Combo Mixto 3",
         price: "129",
-        image: Combo3,
+        image: "https://res.cloudinary.com/dq98mlb66/image/upload/v1666563064/Combo_Mixto_3_emg128.jpg",
         contains: ["Mixto","Cerdo", "Pollo", "Res"]
     },{
         name: "Combo Mixto 4",
         price: "145",
-        image: Combo4,
+        image: "https://res.cloudinary.com/dq98mlb66/image/upload/v1666563062/Combo_Mixto_4_jkymyr.jpg",
         contains: ["Mixto","Pollo", "Cerdo", "Res"]
     },{
         name: "Combo Mixto 5",
         price: "175",
-        image: Combo5,
+        image: "https://res.cloudinary.com/dq98mlb66/image/upload/v1666563063/Combo_Mixto_5_jhssz1.jpg",
         contains: ["Mixto","Pollo", "Res"]
     },{
         name: "Combo Mixto 6",
         price: "195",
-        image: Combo6,
+        image: "https://res.cloudinary.com/dq98mlb66/image/upload/v1666563069/Combo_Mixto_6_zze4cf.jpg",
         contains: ["Mixto","Cerdo", "Pollo", "Res"]
     },{
         name: "Combo Mixto 7",
         price: "205",
-        image: Combo7,
+        image: "https://res.cloudinary.com/dq98mlb66/image/upload/v1666563067/Combo_Mixto_7_cizb0y.jpg",
         contains: ["Mixto","Cerdo", "Pollo", "Res"]
     },{
         name: "Combo Mixto 8",
         price: "240",
-        image: Combo8,
+        image: "https://res.cloudinary.com/dq98mlb66/image/upload/v1666563074/Combo_Mixto_8_hhxnuz.jpg",
         contains: ["Mixto","Pollo", "Res", "Cerdo" ]
     },{
         name: "Combo Mixto 9",
         price: "260",
-        image: Combo9,
+        image: "https://res.cloudinary.com/dq98mlb66/image/upload/v1666563072/Combo_Mixto_9_t8tln6.jpg",
         contains: ["Mixto","Res", "Cerdo"]
     },{
         name: "Combo Básico Pollo",
         price: "115",
-        image: ComboPollo,
+        image: "https://res.cloudinary.com/dq98mlb66/image/upload/v1666563060/Combo_B%C3%A1sico_Pollo_sjxxzj.jpg",
         contains: ["Pollo"]
     },{
         name: "Combo Básico Cerdo",
         price: "115",
-        image: ComboCerdo,
+        image: "https://res.cloudinary.com/dq98mlb66/image/upload/v1666563049/Combo_B%C3%A1sico_Cerdo_qgwllj.jpg",
         contains: ["Cerdo"]
     },{
         name: "Combo Básico Res",
         price: "125",
-        image: ComboRes,
+        image: "https://res.cloudinary.com/dq98mlb66/image/upload/v1666563059/Combo_B%C3%A1sico_Res_tmqzon.jpg",
         contains: ["Res"]
     },{
         name: "Combo Pollo Frito",
         price: "129",
-        image: ComboFrito,
+        image: "https://res.cloudinary.com/dq98mlb66/image/upload/v1666563072/Combo_Pollo_Frito_ljrlcb.jpg",
         contains: ["Pollo"]
     },{
         name: "Combo Aceite Pollo Res",
         price: "190",
-        image: PolloAceitePolloRes,
+        image: "https://res.cloudinary.com/dq98mlb66/image/upload/v1666563050/Combo_Aceite_Pollo_Res_i35gki.jpg",
         contains: ["Pollo", "Res"]
     },
 ]
