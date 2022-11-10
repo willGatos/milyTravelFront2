315c384e0ccd8642f445e4cbb9c0816e8270afba
Bill Gatos <ironmorew@gmail.com>
2022-11-09 23:28:31 -0500

    USER ADMIN

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
 import axios from 'axios';
 
-axios.defaults.baseURL = "http://localhost:3001"
+axios.defaults.baseURL = "https://api.milytravel.net" //http://localhost:3001
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
